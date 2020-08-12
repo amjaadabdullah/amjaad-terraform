d0013337063b6b7d5bcd99b57a4a91d718f6de1d
amjaadabdullah <66108066+amjaadabdullah@users.noreply.github.com>
2020-08-12 17:14:08 +0300

    Update main.tf

--- lab/main.tf
+++ lab/main.tf
@@ -172,6 +172,9 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address = true
   tags                        = module.tags_webserver.tags
   depends_on                  = [aws_instance.api]
+    provisioner "local-exec" {
+      command = "echo ${aws_instance.api.0.public_ip} > p_ip_api.txt"
+      }
 }
 
 resource "aws_instance" "api" {
